e647299859f8a67df75e05ccd77d54c47cba086f
skr18 <sujeetrath123@gmail.com>
2023-04-11 15:18:02 +0530

    signup page

---
+++ signup page with backend/app.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express();
+const PORT =3000
+const authRouter = express.Router();
+app.use(express.json())
+app.use('/auth',authRouter);
+app.listen(PORT,()=>{
+    console.log('server is running')
+});
+authRouter
+.route('/signup')
+.get(getdata)
+.post(postdata)
+
+function getdata(req,res){
+    res.sendFile('/index.html',{root:__dirname })
+}
+
+function postdata(req,res){
+    let obj = req.body
+    res.json({
+        message:"looged in",
+        data:obj
+    });
+    console.log('backend ',obj);
+    res.redirect('/login.html',{root:__dirname })
+}
